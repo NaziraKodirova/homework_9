d1c8f19629cce571c54df55baf8ec777744cf74e
NaziraKodirova <93966535+NaziraKodirova@users.noreply.github.com>
2024-04-17 11:19:04 +0500

    Add files via upload

---
+++ course/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import CourseListView, TeacherListView
+
+urlpatterns = [
+    path('course/',CourseListView.as_view(), name='courses'),
+    path('teachers/',TeacherListView.as_view(), name='teachers')
+]
